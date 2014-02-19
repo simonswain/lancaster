69bb8c863d440627148e4253301887ae38307672
Simon Swain <simon@simonswain.com>
2014-02-20 08:29:37 +1100

    Added routes and tests

--- test/server_test.js
+++ test/server_test.js
@@ -11,13 +11,30 @@ var http = require('nodeunit-httpclient')
     status: 200
   });
 
-var server;
+var server, topo, worker;
 
 exports['server'] = {
 
-  'new-server': function(test) {
+  'new-topo': function(test) {
+    topo = new Lancaster.Topology(config, function(){
+      test.done();
+    });
+  },
 
+  'new-worker': function(test) {
+    worker = new Lancaster.Worker(config, function(){
+      test.done();
+    });
+  },
+
+  'new-server': function(test) {
     server = new Lancaster.Server(config, function(){
+      test.done();
+    });
+  },
+
+  'reset': function(test) {
+    topo.reset(function(){
       test.done();
     });
   },
@@ -34,173 +51,121 @@ exports['server'] = {
     
     http.get( 
       test, 
-      'ping',
+      '',
       function(res) {
-        test.ok(res.data.hasOwnProperty('pong'), 'Got Pong');
+        test.ok(res.data.hasOwnProperty('lancaster'), 'Config');
         test.done();
       });
 
   },
 
   'inspect': function(test) {
-
     test.expect(2);
-
     http.get( test, 'nodes', function(res) {
       test.equal(typeof res.data, 'object');
-      server.stop();
+      test.done();
     });
-   
+    
 
   },
 
   'get-non-existant-node': function(test) {
     test.expect(1);
 
-        http.get( 
-          test, 
-          'nodes/bogus', 
-          {}, 
-          {
-            status: 404
-          }, 
-          function(res) {
-            server.stop();
-          });
+    http.get( 
+      test, 
+      'nodes/bogus', 
+      {}, 
+      {
+        status: 404
+      }, 
+      function(res) {
+        test.done();
       });
-    });
+  },
+
+  'create-node': function(test) {
+    //test.expect(4);
+
+    http.post(
+      test, 
+      'nodes', {
+        data:{
+          'id': 'test-node'
+        }
+      }, {
+        status: 200
+      }, function(res) {
+        // inpsect to check nodes were created
+        http.get( test, 'nodes', function(res) {
+          test.equal(typeof res.data, 'object');
+          test.ok(res.data.hasOwnProperty('test-node'));
+          test.done();
+        });
+        
+      });
     
   },
 
-//   'create-node': function(test) {
-//     test.expect(4);
+  'delete-node': function(test) {
+    test.expect(4);
 
-//     var server = new Lancaster(config);
-
-//     server.on('stop', function(){
-//       test.done();
-//     });
-
-//     server.on('start', function(){
-//       server.reset(function(){
-//         http.post(
-//           test, 
-//           'nodes', {
-//             data:{
-//               'id': 'test-node'
-//             }
-//           }, {
-//             status: 204
-//           }, function(res) {
-
-//             // inpsect to check nodes were created
-//             http.get( test, 'nodes', function(res) {
-//               test.equal(typeof res.data, 'object');
-//               test.ok(res.data.hasOwnProperty('test-node'));
-//               server.stop();
-//             });
-
-//           });
-
-//       });
-//     });
+    http.del(
+      test, 
+      'nodes/test-node', {
+      }, {
+        status: 200
+      }, function(res) {
+        // inpsect to check nodes were created
+        http.get( test, 'nodes', function(res) {
+          test.equal(typeof res.data, 'object');
+          test.equal(res.data.hasOwnProperty('test-node'), false);
+          test.done();
+        });
+        
+      });
     
-//     server.start();
+  },
 
-//   },
 
-//   'delete-node': function(test) {
-//     test.expect(5);
+  'inject': function(test) {
+    //test.expect(5);
+    http.post(
+      test, 
+      'nodes', 
+      {data:{
+        'id': 'test-node', 
+        'factor':10,
+        'fn': 'thru'
+      }},
+      {status: 200}, 
+      function(res) {
+        var myMessage = {value: 1000.00};
 
-//     var server = new Lancaster(config);
-
-//     server.on('stop', function(){
-//       test.done();
-//     });
-
-//     server.on('start', function(){
-//       server.reset(function(){
-//         http.post(
-//           test, 
-//           'nodes', {
-//             data:{
-//               'id': 'test-node'
-//             }
-//           }, {
-//             status: 204
-//           }, function(res) {
-
-//             http.del(
-//               test, 
-//               'nodes/test-node', 
-//               {},
-//               {status: 204},
-//               function(res) {
-
-//                 http.get( test, 'nodes', function(res) {
-//                   test.equal(typeof res.data, 'object');
-//                   test.equal(res.data.hasOwnProperty('test-node'), false);
-//                   server.stop();
-//                 });
-//               });
-
-//           });
-//       });
-//     });
-    
-//     server.start();
-
-//   },
-
-//   'inject': function(test) {
-//     //test.expect(5);
-
-//     var server = new Lancaster(config);
-
-//     server.on('stop', function(){
-//       test.done();
-//     });
-
-//     server.on('start', function(){
-//       server.reset(function(){
-//         http.post(
-//           test, 
-//           'nodes', 
-//           {data:{
-//             'id': 'test-node', 
-//             'factor':10,
-//             'fn': 'multiply'
-//           }},
-//           {status: 204}, 
-//           function(res) {
-//             var myMessage = {value: 1000.00};
-
-//             http.post(
-//               test, 
-//               'nodes/test-node/message', 
-//               {data: myMessage},
-//               {status: 204}, 
-//               function(res) {
-//                 // get latched message
-//                 http.get(
-//                   test, 
-//                   'nodes/test-node', 
-//                   function(res) {
-//                     test.equal(typeof res.data, 'object');
-//                     // should have latched most recent processed
-//                     // message
-//                     test.deepEqual(res.data.message, myMessage);
-//                     server.stop();
-//                   });
-                
-//               });
-//           });
-//       });
-//     });
-    
-//     server.start();
-
-//   },
+        http.post(
+          test, 
+          'nodes/test-node/message', 
+          {data: myMessage},
+          {status: 200}, 
+          function(res) {
+            // get latched message
+            worker.tick(function(err, id, output){            
+              http.get(
+                test, 
+                'nodes/test-node', 
+                function(res) {
+                  var node = res.data;
+                  test.equal(typeof node, 'object');
+                  // should have latched most recent processed
+                  // message
+                  test.equal(node.data.value, myMessage.value);
+                  test.done();
+                });
+              
+            });
+          });
+      });
+  },
 
   'stop': function(test) {
     server.stop(function(){
