10a1e8fecba62b8dccc5910e066e7b337796c0f5
Simon Swain <simon@simonswain.com>
2014-09-25 20:13:14 +1000

    Whitespace

--- tasks/env.js
+++ tasks/env.js
@@ -2,10 +2,9 @@
 
 module.exports = function (grunt) {
   grunt.registerMultiTask(
-    'env', 
-    'Specify an ENV configuration for future tasks in the chain', 
+    'env',
+    'Specify an ENV configuration for future tasks in the chain',
     function() {
       grunt.util._.extend(process.env, this.options(), this.data);
     });
 };
-
